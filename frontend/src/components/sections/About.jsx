ea785399616be0e5303e621c2ee3e8f37d153485
Amir3356 <amirsiraj1995@gmail.com>
2026-05-13 20:13:07 +0300

    djjdjd

--- frontend/src/components/sections/About.jsx
+++ frontend/src/components/sections/About.jsx
@@ -195,7 +195,7 @@ export default function About() {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const highlightBlueClass = "text-blue-400";
+  const highlightBlueClass = "text-gray-700";
 
   useEffect(() => {
     const fetchAboutData = async () => {
