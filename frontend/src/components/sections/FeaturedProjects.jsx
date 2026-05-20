47bd16ab4a5671ba657360f524704d57feca06c3
Developer <developer@betwo.tech>
2026-05-20 16:17:29 +0300

    yyyy

--- frontend/src/components/sections/FeaturedProjects.jsx
+++ frontend/src/components/sections/FeaturedProjects.jsx
@@ -82,7 +82,7 @@ export default function FeaturedProjects() {
           whileInView="visible"
           viewport={{ once: true, amount: 0.2 }}
         >
-          {projects.map((project, index) => (
+          {projects.map((project) => (
             <motion.div
               key={project.id}
               variants={cardVariants}
