089420aac00c218cdbeb60a4065f842bbe6fa74b
Amir3356 <amirsiraj1995@gmail.com>
2026-06-06 08:53:12 +0300

    dgdggddg

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,12 +11,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001',
+        target: process.env.VITE_API_URL || 'http://localhost:5002',
         changeOrigin: true,
         secure: false,
       },
       '/storage': {
-        target: process.env.VITE_API_URL || 'http://localhost:5001',
+        target: process.env.VITE_API_URL || 'http://localhost:5002',
         changeOrigin: true,
         secure: false,
       },
